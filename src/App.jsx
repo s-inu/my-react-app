a599b9dd52cfbffe7007751af225896e1ae5a483
sam <saminusama@gmail.com>
2024-05-06 21:11:49 -0400

    backend443

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   useEffect(() => {
     const fetch = async () => {
-      const res = await axios.get("http://localhost:81");
+      const res = await axios.get("backend://443");
       console.log(res.data);
       setData(res.data);
     };
