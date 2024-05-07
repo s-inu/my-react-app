0bc7d0ec270b87005be7cd7568dbf986f54f7878
sam <saminusama@gmail.com>
2024-05-06 20:06:13 -0400

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await axios.get("http://localhost:81");
+      console.log(res.data);
+      setData(res.data);
+    };
+
+    fetch();
+  }, []);
 
   return (
     <>
+      <div>avcadsfasdf</div>
+      <div>{data && JSON.stringify(data, null, 2)}</div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +43,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
